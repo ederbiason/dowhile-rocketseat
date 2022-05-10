7979c924096cf40216b0ab197c27a657de89ab31
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-05-10 15:09:56 -0300

    return 3 last messages

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateMessageController } from "./controllers/CreateMessageController";
+import { GetLast3MessagesController } from "./controllers/GetLast3MessagesController";
 import { ensureAuthentication } from "./middleware/ensureAuthenticate";
 
 const router = Router()
@@ -11,6 +12,8 @@ router.post(
     "/messages", 
     ensureAuthentication, 
     new CreateMessageController().handle
-    )
+)
+
+router.get("/messages/last3", new GetLast3MessagesController().handle)
 
 export { router }
